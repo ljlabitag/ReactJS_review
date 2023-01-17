62d4a2f0f4a2b96353f45551c1887c9b98f935b2
ljlabitag <ljlabitagdev@gmail.com>
2023-01-17 20:32:56 +0800

    save code edits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react'
+
+const CounterWithNameAndSideEffect = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log(`You clicked ${count} times`)
+  })
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+
+function WelcomeMessage() {
+  return(<p>Welcome!</p>)
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +30,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <WelcomeMessage/>
+        <CounterWithNameAndSideEffect/>
         <a
           className="App-link"
           href="https://reactjs.org"
